580c41cf39580c6eccf44a66d3c6a2a003a8c752
Ashenafi-Tesfaye <65870089+Ashenafi-Tesfaye@users.noreply.github.com>
2024-05-22 17:29:43 -0400

    added tests

--- src/test/java/SWEN656AGcom/example/factoryMethodDemo/FactoryMethodDemoApplicationTests.java
+++ src/test/java/SWEN656AGcom/example/factoryMethodDemo/FactoryMethodDemoApplicationTests.java
@@ -1,13 +1,40 @@
 package SWEN656AGcom.example.factoryMethodDemo;
 
+import SWEN656AGcom.example.factoryMethodDemo.logistics.*;
+import SWEN656AGcom.example.factoryMethodDemo.transport.Airplane;
+import SWEN656AGcom.example.factoryMethodDemo.transport.Ship;
+import SWEN656AGcom.example.factoryMethodDemo.transport.Truck;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class FactoryMethodDemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testRoadLogistics() {
+		Logistics logistics = new RoadLogistics();
+		Transport transport = logistics.createTransport();
+		assertTrue(transport instanceof Truck, "Expected transport to be an instance of Truck");
+	}
+
+	@Test
+	void testSeaLogistics() {
+		Logistics logistics = new SeaLogistics();
+		Transport transport = logistics.createTransport();
+		assertTrue(transport instanceof Ship, "Expected transport to be an instance of Ship");
+	}
+
+	@Test
+	void testAirLogistics() {
+		Logistics logistics = new AirLogistics();
+		Transport transport = logistics.createTransport();
+		assertTrue(transport instanceof Airplane, "Expected transport to be an instance of Airplane");
 	}
 
 }
